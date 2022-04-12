932ebe44dff85e96cd5486b8004ace6bd212bc49
20121a1522@MNSDOMAIN.COM <20121a1522@MNSDOMAIN.COM>
2022-04-12 11:36:37 +0530

    sena

---
+++ week1a.py
@@ -0,0 +1,13 @@
+print("enter two number:");
+a=int(input("enter a value :"));
+b=int(input("enter b value :"));
+add=a+b;
+sub=a-b;
+mult=a*b;
+divi=a/b;
+mod=a%b;
+print(add);
+print(sub);
+print(mult);
+print(divi);
+print(mod);
